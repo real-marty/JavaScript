8171e297a46505bc85832e3db76746d677850c76
marty <riedlmarty@gmail.com>
2022-02-03 21:59:40 +0100

    Added colors

---
+++ Colors Project/app.js
@@ -0,0 +1,59 @@
+// Global variables
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+let initialColors;
+
+// Functions
+function generateHex() {
+    const hexColor = chroma.random();
+
+    return hexColor;
+}
+
+// Color generator
+function radomColors() {
+    colorDivs.forEach((div, index) => {
+        const hexText = div.children[0];
+        const randomColor = generateHex();
+        // add clr to the background
+        div.style.backgroundColor = randomColor;
+        hexText.innerText = randomColor;
+        //check for contrast
+        checkTextContrast(randomColor, hexText);
+        // initial colorize sliders
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll(" .sliders input");
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color, hue, brightness, saturation);
+    });
+}
+function checkTextContrast(color, text) {
+    const luminance = chroma(color).luminance();
+    if (luminance > 0.5) {
+        text.style.color = "black"
+    }
+    else {
+        text.style.color = "white"
+    }
+}
+function colorizeSliders(color, hue, brightness, saturation) {
+    // scale saturation
+    const noSat = color.set("hsl.s", 0);
+    const fullSat = color.set("hsl.s", 1);
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+    // scale brightness
+    const midBright = color.set("hsl.l", 0.5);
+    const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+    // input update
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`;
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(0)}, ${scaleBright(0.5)}, ${scaleBright(1)})`;
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75), rgb(75,204,204), rgb(75,75,204), rgb(204,75,204), rgb(204,75,75) )`
+}
+
+radomColors();
