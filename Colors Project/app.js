c4478c940c948ff9540860886d1b1573afd8b10a
marty <riedlmarty@gmail.com>
2022-02-04 16:52:15 +0100

    integrating liders

--- Colors Project/app.js
+++ Colors Project/app.js
@@ -3,8 +3,28 @@ const colorDivs = document.querySelectorAll(".color");
 const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
+const popup = document.querySelector('.copy-container')
 let initialColors;
+//event listener
+sliders.forEach(slider => {
+    slider.addEventListener("input", hslControls);
+});
+colorDivs.forEach((div, index) => {
+    div.addEventListener("change", () => {
+        updateTextUI(index);
+    })
+});
 
+currentHexes.forEach(hex => {
+    hex.addEventListener('click', () => {
+        copyToClipboard(hex);
+    })
+});
+popup.addEventListener('transitionend', () => {
+    const popupBox = popup.children[0];
+    popup.classList.remove('active');
+    popupBox.classList.remove('active');
+})
 // Functions
 function generateHex() {
     const hexColor = chroma.random();
@@ -14,9 +34,11 @@ function generateHex() {
 
 // Color generator
 function radomColors() {
+    initialColors = [];
     colorDivs.forEach((div, index) => {
         const hexText = div.children[0];
         const randomColor = generateHex();
+        initialColors.push(chroma(randomColor).hex());
         // add clr to the background
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
@@ -30,7 +52,10 @@ function radomColors() {
         const saturation = sliders[2];
 
         colorizeSliders(color, hue, brightness, saturation);
+
     });
+    // reset inputs
+    resetInputs();
 }
 function checkTextContrast(color, text) {
     const luminance = chroma(color).luminance();
@@ -54,6 +79,65 @@ function colorizeSliders(color, hue, brightness, saturation) {
     saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`;
     brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(0)}, ${scaleBright(0.5)}, ${scaleBright(1)})`;
     hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75), rgb(75,204,204), rgb(75,75,204), rgb(204,75,204), rgb(204,75,75) )`
+}
+function hslControls(e) {
+    const index =
+        e.target.getAttribute('data-bright') ||
+        e.target.getAttribute('data-sat') ||
+        e.target.getAttribute('data-hue');
+    let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+    const bgColor = initialColors[index];
+    let color = chroma(bgColor)
+        .set('hsl.s', saturation.value)
+        .set('hsl.l', brightness.value)
+        .set('hsl.h', hue.value);
+    colorDivs[index].style.backgroundColor = color;
+    // colorize sliders
+    colorizeSliders(color, hue, brightness, saturation);
+
+}
+function updateTextUI(index) {
+    const activeDiv = colorDivs[index];
+    const color = chroma(activeDiv.style.backgroundColor);
+    const textHex = activeDiv.querySelector('h2');
+    const icons = activeDiv.querySelectorAll('.controls button');
+    textHex.innerText = color.hex();
+    // check contrast and chnge text collor
+    checkTextContrast(color, textHex);
+    for (icon of icons) {
+        checkTextContrast(color, icon);
+
+    }
+}
+function resetInputs() {
+    const sliders = document.querySelectorAll('.sliders input');
+    sliders.forEach(slider => {
+        if (slider.name === 'hue') {
+            const hueColor = initialColors[slider.getAttribute('data-hue')];
+            const hueValue = chroma(hueColor).hsl()[0];
+            slider.value = Math.floor(hueValue);
+        }
+        if (slider.name === 'brightness') {
+            const brightColor = initialColors[slider.getAttribute('data-bright')];
+            const brightValue = chroma(brightColor).hsl()[2];
+            slider.value = brightValue * 100 / 100;
+        }
+        if (slider.name === 'saturation') {
+            const satColor = initialColors[slider.getAttribute('data-sat')];
+            const satValue = chroma(satColor).hsl()[1];
+            slider.value = satValue * 100 / 100;
+        }
+    });
+
+}
+function copyToClipboard(hex) {
+    navigator.clipboard.writeText(hex.innerText);
+    const popupBox = popup.children[0];
+    popup.classList.toggle('active');
+    popupBox.classList.add('active');
 }
 
 radomColors();
